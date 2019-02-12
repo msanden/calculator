e9b776827546dc430dbf396cb1e4849fd1036b9f
Manuel Sande <mnlsande22@gmail.com>
2019-02-12 11:11:18 -0500

    Correct url

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,12 @@ var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1,number2);
 alert(result);
+
+function subtract(number1, number2) {
+  return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = subtract(number1,number2);
+alert(result);
